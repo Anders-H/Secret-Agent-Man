98246e02b753162aab5500fc2e09e84b7a6af1cb
Anders Hesselbom <anders@winsoft.se>
2025-01-23 18:07:05 +0100

    Checking for .NET 8.

--- SecretAgentMan/InstallSAM/MainWindow.Designer.cs
+++ SecretAgentMan/InstallSAM/MainWindow.Designer.cs
@@ -29,24 +29,70 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(8, 8);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(512, 68);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Checking .NET versions...";
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblUrl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblUrl.Location = new System.Drawing.Point(4, 80);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(278, 13);
+            this.lblUrl.TabIndex = 1;
+            this.lblUrl.Text = "https://dotnet.microsoft.com/en-us/download/dotnet/8.0";
+            this.lblUrl.Visible = false;
+            this.lblUrl.Click += new System.EventHandler(this.lblUrl_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Location = new System.Drawing.Point(440, 120);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 396);
+            this.ClientSize = new System.Drawing.Size(523, 149);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.lblMessage);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Install Secret Agent Man";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.MainWindow_Shown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- SecretAgentMan/InstallSAM/MainWindow.cs
+++ SecretAgentMan/InstallSAM/MainWindow.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InstallSAM
@@ -15,6 +11,66 @@ namespace InstallSAM
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Shown(object sender, EventArgs e)
+        {
+            Refresh();
+
+            if (HasDotNet8())
+            {
+                lblMessage.Text = @".NET 8 is installed.";
+                Refresh();
+                System.Threading.Thread.Sleep(1000);
+                Close();
+                return;
+            }
+            else
+            {
+                lblMessage.Text = @"You need to install .NET Desktop Runtime 8. Find the section called "".NET Desktop Runtime"", and click on the installer link to the correct version. If you are unsure, it might be x64 you are looking for.";
+                lblUrl.Visible = true;
+                btnClose.Enabled = true;
+            }
+        }
+
+        private bool HasDotNet8()
+        {
+            try
+            {
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "dotnet.exe",
+                        Arguments = "--list-runtimes",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                proc.Start();
+                var rows = new List<string>();
+
+                while (!proc.StandardOutput.EndOfStream)
+                    rows.Add(proc.StandardOutput.ReadLine());
+
+                return rows.Any(row => row.StartsWith("Microsoft.WindowsDesktop.App 8.", StringComparison.CurrentCultureIgnoreCase));
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lblUrl_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(lblUrl.Text);
         }
     }
 }
