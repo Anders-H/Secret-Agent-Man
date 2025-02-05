fb5c04a6b0732023baacf3b2a938599222224fe1
Anders Hesselbom <anders@winsoft.se>
2025-02-05 19:41:55 +0100

    Started to work on a gunfire sprite.

--- SecretAgentMan/SecretAgentMan/Fire.cs
+++ SecretAgentMan/SecretAgentMan/Fire.cs
@@ -1,5 +1,48 @@
-﻿namespace SecretAgentMan;
+﻿using Microsoft.Xna.Framework.Graphics;
+using RetroGame;
+using RetroGame.Scene;
+using RetroGame.Sprites;
 
-public class Fire
+namespace SecretAgentMan;
+
+public class Fire : Sprite, IRetroActor
 {
+    private const int Speed = 1;
+    private readonly int[] _player = [16, 17];
+    private readonly int[] _enemy = [18, 19];
+    private readonly int[] _currentAnimation;
+    private int _currentAnimationIndex;
+    private readonly bool _faceRight;
+
+    public Fire(bool faceRight, bool isEnemy)
+    {
+        _faceRight = faceRight;
+        _currentAnimation = isEnemy ? _enemy : _player;
+        _currentAnimationIndex = 0;
+    }
+
+    public void Act(ulong ticks)
+    {
+        if (ticks % 2 == 0)
+        {
+            _currentAnimationIndex++;
+
+            if (_currentAnimationIndex > 1)
+                _currentAnimationIndex = 0;
+        }
+
+        if (_faceRight)
+            X += Speed;
+        else
+            X -= Speed;
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        Game1.CharactersTexture.Draw(spriteBatch, _currentAnimation[_currentAnimationIndex], (int)X, (int)Y, ColorPalette.White);
+    }
+
+    public bool IsDead =>
+        X < -25 || X > 639;
+
 }
