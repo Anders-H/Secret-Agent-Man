74058b0b064a72fa97d29cf46e48b9b2985eae7f
Anders Hesselbom <anders@winsoft.se>
2025-04-12 17:21:26 +0200

    Issue #6: Not sure what decoration graphics to use on room 1 and 2

--- SecretAgentMan/SecretAgentMan/OtherResources/Decoration.cs
+++ SecretAgentMan/SecretAgentMan/OtherResources/Decoration.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
-using RetroGame;
+using RetroGame.RetroTextures;
 using RetroGame.Sprites;
 
 namespace SecretAgentMan.OtherResources;
@@ -10,47 +10,65 @@ public class Decoration : Sprite
     {
         switch (roomIndex)
         {
+            case 0:
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 0, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 3, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 0, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 3, 0, 0, Flip.FlipLeftRight);
+                break;
+            case 1:
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 2, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 2, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 1, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 2, 0, 0, Flip.FlipLeftRight);
+                break;
+            case 2:
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 3, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 1, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 2, 0, 0, Flip.FlipLeftRight);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 1, 0, 0, Flip.FlipLeftRight);
+                break;
             case 3:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 3, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 0, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 1, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 3, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 0, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 2, 0, 0);
                 break;
             case 4:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 3, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 1, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 2, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 1, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 3, 0, 0);
                 break;
             case 5:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 0, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 0, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 1, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 2, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 1, 0, 0);
                 break;
             case 6:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 0, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 3, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 0, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 2, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 0, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 3, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 0, 0, 0);
                 break;
             case 7:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 3, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 0, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 3, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 1, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 0, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 1, 0, 0);
                 break;
             case 8:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 0, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 1, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 0, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 2, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 1, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 2, 0, 0);
                 break;
             case 9:
-                Game1.BackgroundLayer01!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer02!.Draw(spriteBatch, 3, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer03!.Draw(spriteBatch, 2, 0, 0, ColorPalette.White);
-                Game1.BackgroundLayer04!.Draw(spriteBatch, 3, 0, 0, ColorPalette.White);
+                Game1.BackgroundLayer01!.Draw(spriteBatch, 2, 0, 0);
+                Game1.BackgroundLayer02!.Draw(spriteBatch, 3, 0, 0);
+                Game1.BackgroundLayer03!.Draw(spriteBatch, 2, 0, 0);
+                Game1.BackgroundLayer04!.Draw(spriteBatch, 3, 0, 0);
                 break;
         }
     }
