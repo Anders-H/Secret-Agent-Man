474fd6afe8358691e19c98bbfc25bd143ada71fd
Anders Hesselbom <anders@winsoft.se>
2026-04-18 16:22:38 +0200

    #10 Installationsprogrammet fungerar.

--- SecretAgentMan/GenerateFileListForSetup/Form1.cs
+++ SecretAgentMan/GenerateFileListForSetup/Form1.cs
@@ -12,8 +12,10 @@ public partial class Form1 : Form
     private void Form1_Load(object sender, EventArgs e)
     {
         var s = new StringBuilder();
-        s.AppendLine(@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\PublishedProgram\{#MyAppExeName}""; DestDir: ""{app}""; Flags: ignoreversion");
-        var gameDirectory = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\PublishedProgram\");
+        s.AppendLine("// Main exe");
+        s.AppendLine(@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\{#MyAppExeName}""; DestDir: ""{app}""; Flags: ignoreversion");
+        var gameDirectory = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\");
+        s.AppendLine("//Supporting files");
 
         foreach (var fileInfo in gameDirectory.GetFiles())
         {
@@ -26,10 +28,11 @@ public partial class Form1 : Form
             if (string.Compare(fileInfo.Name, "cheat.dat", StringComparison.CurrentCultureIgnoreCase) == 0)
                 continue;
 
-            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\PublishedProgram\{fileInfo.Name}""; DestDir: ""{{app}}""; Flags: ignoreversion");
+            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\{fileInfo.Name}""; DestDir: ""{{app}}""; Flags: ignoreversion");
         }
 
-        var contentDirectory = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\PublishedProgram\Content\");
+        s.AppendLine("// Main content");
+        var contentDirectory = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\");
 
         foreach (var fileInfo in contentDirectory.GetFiles())
         {
@@ -39,7 +42,31 @@ public partial class Form1 : Form
             if (fileInfo.Name.EndsWith(".pdb", StringComparison.CurrentCultureIgnoreCase))
                 continue;
 
-            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\PublishedProgram\Content\{fileInfo.Name}""; DestDir: ""{{app}}\Content""; Flags: ignoreversion");
+            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\{fileInfo.Name}""; DestDir: ""{{app}}\Content""; Flags: ignoreversion");
+        }
+
+        s.AppendLine("// Background buildings");
+        var backgrounds = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\bg");
+
+        foreach (var fileInfo in backgrounds.GetFiles())
+        {
+            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\bg\{fileInfo.Name}""; DestDir: ""{{app}}\Content\bg""; Flags: ignoreversion");
+        }
+
+        s.AppendLine("// Background silhouettes");
+        var silhouettes = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\bg\bg");
+
+        foreach (var fileInfo in silhouettes.GetFiles())
+        {
+            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\bg\bg\{fileInfo.Name}""; DestDir: ""{{app}}\Content\bg\bg""; Flags: ignoreversion");
+        }
+
+        s.AppendLine("// Background skies");
+        var sky = new DirectoryInfo(@"D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\bg\sky");
+
+        foreach (var fileInfo in sky.GetFiles())
+        {
+            s.AppendLine($@"Source: ""D:\GitRepos\Secret-Agent-Man\SecretAgentMan\SecretAgentMan\bin\Release\net8.0-windows\win-x64\Content\bg\sky\{fileInfo.Name}""; DestDir: ""{{app}}\Content\bg\sky""; Flags: ignoreversion");
         }
 
         textBox1.Text = s.ToString();
